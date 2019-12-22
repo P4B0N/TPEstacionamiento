2675add5736bdea88e7eb4c06b0b89a425ba2a02
Your Name <you@example.com>
2019-12-21 21:15:39 -0300

    acceso a bd

--- funciones/traerdatos.php
+++ funciones/traerdatos.php
@@ -1,7 +1,7 @@
 <?php
 include 'accesoadatos.php';
 	//session_start();
-var_dump("hola")die();
+var_dump("jklghjlkgljjgfjl");die();
 			$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); //obtener consulta
 			$consulta =$objetoAccesoDato->RetornarConsulta("select nombre  , clave  from usuario"); //crear consulta
 			$consulta->execute();	//ejecutar
