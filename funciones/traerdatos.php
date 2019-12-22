0ba526b4cd0c2bab3452f97ecffbe78f73679eee
Your Name <you@example.com>
2019-12-21 21:06:14 -0300

    acceso a bd

--- funciones/traerdatos.php
+++ funciones/traerdatos.php
@@ -1,18 +1,18 @@
 <?php
 include 'accesoadatos.php';
 	session_start();
-			$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
-			$consulta =$objetoAccesoDato->RetornarConsulta("select nombre  , clave  from usuario");
-			$consulta->execute();			
-			$datos= $consulta->fetchAll(PDO::FETCH_ASSOC);		
+			$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); //obtener consulta
+			$consulta =$objetoAccesoDato->RetornarConsulta("select nombre  , clave  from usuario"); //crear consulta
+			$consulta->execute();	//ejecutar
+			$datos= $consulta->fetchAll(PDO::FETCH_ASSOC);		//traer al formato esperado
 			//var_dump($datos);
 			foreach ($datos as $usuario) 
 			{
-				//var_dump($usuario);
-				echo ($usuario["nombre"]);
-				echo "<br>";
-				echo($usuario["clave"]);
-				echo "<br>";
+			var_dump($usuario);
+				//echo ($usuario["nombre"]);
+				//echo "<br>";
+				//echo($usuario["clave"]);
+				//echo "<br>";
 			}
 	
 ?>
